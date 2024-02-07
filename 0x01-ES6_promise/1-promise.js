89bf53dfeeeaff015b11e416f30bf0a6211fd82b
raytchellee <amakaoforkaja@gmail.com>
2024-02-07 22:23:40 +0000

    checking

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+export default function getFullResponseFromAPI(success) {
+  const response = new Promise((res, rej) => {
+    if (success) {
+      res({ status: 200, body: 'Success' });
+    } else {
+      rej(new Error('The fake API is not working currently'));
+    }
+  });
+  return response;
+}
