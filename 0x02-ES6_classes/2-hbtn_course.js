6f26f5e255840ae3cfd981cdf570ec40c9f3dabc
raytchellee <amakaoforkaja@gmail.com>
2024-02-08 22:16:01 +0000

    check

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,48 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  set length(len) {
+    if (typeof len !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = len;
+  }
+
+  set students(arr) {
+    if (!Array.isArray(arr)) {
+      throw new TypeError('must be a array');
+    }
+    this._students = arr;
+  }
+}
---
+++ 0x02-ES6_classes/README.md
@@ -0,0 +1,1 @@
+A repo for js Classes
