472db87e4b4b548355151af5bce3ce2bbd9e3362
raytchellee <amakaoforkaja@gmail.com>
2024-02-08 22:25:05 +0000

    check

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+export default class Building {
+  constructor(sqft = 0) {
+    if (this.constructor !== Building
+        && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
