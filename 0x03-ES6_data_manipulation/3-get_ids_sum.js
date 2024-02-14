388d29a3ff59f12865786905c9c9762df9359b25
Ray Ray <101654980+Raytchellee@users.noreply.github.com>
2024-02-14 06:38:48 +0100

    Update 3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,8 @@
-export default const getStudentIdsSum = (data) => {
+const getStudentIdsSum = (data) => {
   const sum = data.reduce(
     (item, next) => item + next.id, 0,
   );
   return sum;
 };
+
+export default getStudentIdsSum;
