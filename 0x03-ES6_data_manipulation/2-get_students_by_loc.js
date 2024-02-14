5b646794f9a8c8926bb90f751733e3b0cba54dae
Ray Ray <101654980+Raytchellee@users.noreply.github.com>
2024-02-14 06:38:22 +0100

    Update 2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,6 @@
-export default const getStudentsByLocation = (data, city) => {
+const getStudentsByLocation = (data, city) => {
   const res = data.filter((item) => item.location === city);
   return res;
 };
+
+export default getStudentsByLocation;
