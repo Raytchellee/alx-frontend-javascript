609e3d86a50a1fe9d2ef41ea5e8e6ea18e2e4889
Ray Ray <101654980+Raytchellee@users.noreply.github.com>
2024-02-14 06:43:09 +0100

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,11 @@
-function updateStudentGradeByCity(students, city, newGrades) {
+export default function updateStudentGradeByCity(students, city, newGrades) {
   if (!Array.isArray(students) || !Array.isArray(newGrades)) {
     return [];
   }
 
-  const filteredStudents = students.filter((student) => student.location === city).map((student) => {
+  const f_s = students.filter((student) => student.location === city).map((student) => {
     const foundData = newGrades.find((item) => student.id === item.studentId);
     return { ...student, grade: foundData ? foundData.grade : 'N/A' };
   });
-  return filteredStudents;
+  return f_s;
 }
-
-export default updateStudentGradeByCity;
